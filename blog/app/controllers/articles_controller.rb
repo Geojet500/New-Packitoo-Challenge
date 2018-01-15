50cd7a8c7e41113a0f0458a8cc8dc385247962dc
Geojet500 <tom.viaud@viacesi.fr>
2018-01-15 14:20:42 -0800

    Basic Authentication

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "tom", password: "root", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
