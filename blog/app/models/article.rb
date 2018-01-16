f3b9ccec79a01643e5e47f027ff7ca7f4d508108
Geojet500 <tom.viaud@viacesi.fr>
2018-01-16 02:40:09 -0800

    Basic text rating

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :ratings
+
   validates :title, :text, presence: true,
-                    length: { minimum: 5 }
+    length: { minimum: 5 }
+  
 
 end
