0339a59665916ffd7864d5be466e8ef545a2ebaa
Geojet500 <tom.viaud@viacesi.fr>
2018-01-16 01:40:03 -0800

    Generating the 'Rating' model

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180115191248) do
+ActiveRecord::Schema.define(version: 20180116093829) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20180115191248) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.string "user"
+    t.integer "score"
+    t.integer "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_ratings_on_article_id"
   end
 
 end
